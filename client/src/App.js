23f7cfa4fd319de5516917e52f1da31495b08982
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-18 18:59:14 -0300

    Criando interface de cadastro

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-
+    <div className='app-container'>
+      <div className='register-container'>
+        <h1 className='register-title'>Crud Movies</h1>
+        <input type='text' name='name' placeholder=' Nome:' className='register-input'></input>
+        <input type='text' name='cost' placeholder=' Preço:' className='register-input'></input>
+        <input type='text' name='category' placeholder=' Categoria:' className='register-input'></input>
+        <button className='register-button'>Cadastrar</button>
+      </div>
     </div>
   );
 }
-
-export default App;
